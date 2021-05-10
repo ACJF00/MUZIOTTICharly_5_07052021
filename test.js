80f8e40974b62075d8dd28e52b5f702296ff13a9
ACJF00 <chrlmztt@gmail.com>
2021-05-10 08:07:56 +0200

    Premiereres fonctions tests

---
+++ test.js
@@ -0,0 +1,73 @@
+const nounourses = [
+  {
+    colors: ["Tan", "Chocolate", "Black", "White"],
+    _id: "5be9c8541c9d440000665243",
+    name: "Norbert",
+    price: 2900,
+    imageUrl: "http://oc-p5-api.herokuapp.com/images/teddy_1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    colors: ["Pale brown", "Dark brown", "White"],
+    _id: "5beaa8bf1c9d440000a57d94",
+    name: "Arnold",
+    price: 3900,
+    imageUrl: "http://oc-p5-api.herokuapp.com/images/teddy_2.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    colors: ["Brown"],
+    _id: "5beaaa8f1c9d440000a57d95",
+    name: "Lenny and Carl",
+    price: 5900,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "http://oc-p5-api.herokuapp.com/images/teddy_3.jpg",
+  },
+  {
+    colors: ["Brown", "Blue", "Pink"],
+    _id: "5beaabe91c9d440000a57d96",
+    name: "Gustav",
+    price: 4500,
+    imageUrl: "http://oc-p5-api.herokuapp.com/images/teddy_4.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    colors: ["Beige", "Tan", "Chocolate"],
+    _id: "5beaacd41c9d440000a57d97",
+    name: "Garfunkel",
+    price: 5500,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "http://oc-p5-api.herokuapp.com/images/teddy_5.jpg",
+  },
+];
+function afficherNomDesNounours(array) {
+  for (let elem of array) {
+    console.log(elem.name);
+  }
+}
+
+afficherNomDesNounours(nounourses);
+
+function afficherPrixTotal(array) {
+    let sum = 0;
+  for (let elem of array) {
+    sum += elem.price;
+  }
+  console.log('Prix total',sum);
+}
+
+afficherPrixTotal(nounourses);
+
+
+function afficherColorisDisponibles(array) {
+    for (let elem of array) {
+        console.log(elem.colors);
+      }
+}
+
+afficherColorisDisponibles(nounourses)
